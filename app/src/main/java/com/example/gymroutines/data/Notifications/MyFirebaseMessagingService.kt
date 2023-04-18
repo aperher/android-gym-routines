7618c532be987fe54e8f051a155328b487c06436
Johnny <johnny.altes1@gmail.com>
2023-04-18 16:30:47 +0200

    Inico Chat

---
+++ app/src/main/java/com/example/gymroutines/data/Notifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,6 @@
+package com.example.gymroutines.data.Notifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+}
