810b6b0b2b5e130c17cdee8ecb824e64938a2c5c
Johnny <johnny.altes1@gmail.com>
2023-05-15 13:13:22 +0200

    Se comenta todo los métodos públicos de data

--- app/src/main/java/com/example/gymroutines/data/auth/AuthDataSourceImpl.kt
+++ app/src/main/java/com/example/gymroutines/data/auth/AuthDataSourceImpl.kt
@@ -14,17 +14,34 @@ class AuthDataSourceImpl @Inject constructor(
         const val USER_COLLECTION = "users"
     }
 
+    /**
+     * Método para crear un usuario en la base de datos
+     * @param user Usuario a crear
+     * @return True si se ha creado correctamente, false en caso contrario
+     */
     override suspend fun createUser(user: User): Boolean = runCatching {
         val account = auth.createUserWithEmailAndPassword(user.email, user.password).await()
         if (account != null)
             db.collection(USER_COLLECTION).document(user.email).set(user).await()
     }.isSuccess
 
+    /**
+     * Metodo para iniciar sesion
+     * @param email Correo del usuario
+     * @param password Contraseña del usuario
+     * @return True si se ha iniciado sesion correctamente, false en caso contrario
+     */
     override suspend fun login(email: String, password: String): Boolean = runCatching {
         auth.signInWithEmailAndPassword(email, password).await()
     }.isSuccess
 
+    /**
+     * Metodo para cerrar sesion
+     */
     override fun logout() = auth.signOut()
 
+    /**
+     *Método para obtener el usuario actual
+     */
     override fun getUser(): String = auth.currentUser?.email ?: ""
 }
--- app/src/main/java/com/example/gymroutines/data/routinesCatalog/RoutinesCatalogRepositoryImpl.kt
+++ app/src/main/java/com/example/gymroutines/data/routinesCatalog/RoutinesCatalogRepositoryImpl.kt
@@ -11,7 +11,10 @@ import javax.inject.Inject
 
 class RoutinesCatalogRepositoryImpl @Inject constructor(private val dataSource: RoutinesCatalogDataSource) :
     RoutinesCatalogRepository {
-
+    /**
+     * Metodo para obtener las rutinas del catalogo de la base de datos
+     * @return Lista de catalogos
+     */
     override fun getRoutinesCatalog(): Flow<List<Catalog>> {
         return dataSource.getRoutinesCatalog().map { catalogList ->
             catalogList.map { catalogDto ->
@@ -20,19 +23,30 @@ class RoutinesCatalogRepositoryImpl @Inject constructor(private val dataSource: 
         }
     }
 
-    override suspend fun getRoutinesByCatalog(catalogTitle: CatalogType): Result<List<RoutinePreview>> = runCatching {
-        dataSource.getRoutinesByCatalog(catalogTitle).let { result ->
-            return if (result.isSuccess) {
-                Result.success(result.getOrThrow().map { routinePreviewDto ->
-                    routinePreviewDto.toDomain()
-                })
-            } else {
-                Result.failure(Exception("Error getting routines by catalog"))
+    /**
+     * Método para obtener las rutinas de un catalogo de la base de datos
+     * @param catalogTitle Titulo del catalogo
+     * @return Lista de rutinas
+     */
+    override suspend fun getRoutinesByCatalog(catalogTitle: CatalogType): Result<List<RoutinePreview>> =
+        runCatching {
+            dataSource.getRoutinesByCatalog(catalogTitle).let { result ->
+                return if (result.isSuccess) {
+                    Result.success(result.getOrThrow().map { routinePreviewDto ->
+                        routinePreviewDto.toDomain()
+                    })
+                } else {
+                    Result.failure(Exception("Error getting routines by catalog"))
+                }
             }
         }
-    }
 
-    override suspend fun getFilteredRoutines(filters : Map<FilterType, MutableList<String>>) : Result<List<RoutinePreview>> =
+    /**
+     * Método para obtener rutinas en función de los filtros
+     * @param filters Filtros a aplicar
+     * @return Lista de rutinas filtradas
+     */
+    override suspend fun getFilteredRoutines(filters: Map<FilterType, MutableList<String>>): Result<List<RoutinePreview>> =
         dataSource.getFilteredRoutines(filters).let { result ->
             return if (result.isSuccess) {
                 Result.success(result.getOrThrow().map { routinePreviewDto ->
@@ -43,6 +57,11 @@ class RoutinesCatalogRepositoryImpl @Inject constructor(private val dataSource: 
             }
         }
 
+    /**
+     * Método para acceder obtener las rutinas en funcion de el nombre
+     * @param title Titulo de la rutina
+     * @return Lista de rutinas
+     */
     override suspend fun searchRoutines(title: String): Result<List<RoutinePreview>> {
         dataSource.searchRoutines(title).let { result ->
             return if (result.isSuccess) {
